77be5672743006dabf30548ccd6d5e47e48fc90f
Jeswin Wilson <jeswinwilson13@gmail.com>
2025-04-25 10:43:42 +0530

    configure cors in backend

--- server/index.js
+++ server/index.js
@@ -14,7 +14,12 @@ const userRoutes = require('./routes/userRoutes');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? [process.env.FRONTEND_URL] 
+    : 'http://localhost:3000',
+  credentials: true
+}));
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
